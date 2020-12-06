10c4c84565fb97c5cb1957773ea15a403666dbcd
zuodengpeng <cqzuodp@163.com>
2020-12-06 10:58:51 +0800

    fix:调整结构.

--- main.go
+++ main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
-    "github.com/go-lib/pkg/fsutils"
-    "github.com/go-lib/pkg/log"
-    "github.com/go-lib/pkg/strutils"
+	"github.com/go-lib/pkg/fsutils"
+	"github.com/go-lib/pkg/log"
+	"github.com/go-lib/pkg/strutils"
+	"github.com/go-lib/pkg/timeutils"
 )
 
+func testFunc() {
+	log.Debugf("infof ====> %s", "ionfo")
+	log.Warnf("infof ====> %s", "ionfo")
+	log.Infof("infof ====> %s", "ionfo")
+	log.Errorf("infof ====> %s", "ionfo")
+
+	e := fsutils.WriteFile("/tmp/a.log", strutils.String2Bytes("aaaaabbbbbb\n"), true)
+	if e != nil {
+		log.Errorf("write file error: %v", e)
+	}
+}
+
 func main() {
-    log.InitConsoleLogger(log.DebugLevel)
+	log.InitConsoleLogger(log.DebugLevel)
 
-    log.Debugf("infof ====> %s", "ionfo")
-    log.Warnf("infof ====> %s", "ionfo")
-    log.Infof("infof ====> %s", "ionfo")
-    log.Errorf("infof ====> %s", "ionfo")
-
-    e := fsutils.WriteFile("/tmp/a.log", strutils.String2Bytes("aaaaabbbbbb\n"), true)
-    if e != nil {
-        log.Errorf("write file error: %v", e)
-    }
+	w := timeutils.NewStopwatch("test")
+	w.TrackStage("func1", testFunc)
+	w.TrackStage("func2", testFunc)
+	w.TrackStage("func3", testFunc)
+	w.PrintStages()
 }
--- pkg/fsutils/fsutils.go
+++ pkg/fsutils/fsutils.go
@@ -1,85 +1,85 @@
 package fsutils
 
 import (
-    "io"
-    "os"
-    "syscall"
+	"io"
+	"os"
+	"syscall"
 )
 
 const (
-    FILE_MODE = 0644
-    DIR_MODE  = 0755
+	FILE_MODE = 0644
+	DIR_MODE  = 0755
 )
 
 func IsDir(filename string) bool {
-    fi, err := sysStat(filename)
-    return err == nil && fileMode(fi) == os.ModeDir
+	fi, err := sysStat(filename)
+	return err == nil && fileMode(fi) == os.ModeDir
 }
 
 func IsSymLink(filename string) bool {
-    fi, err := sysStat(filename)
-    return err == nil && fileMode(fi) == os.ModeSymlink
+	fi, err := sysStat(filename)
+	return err == nil && fileMode(fi) == os.ModeSymlink
 }
 
 func sysStat(filename string) (*syscall.Stat_t, error) {
-    var fs syscall.Stat_t
-    err := syscall.Stat(filename, &fs)
-    if err != nil {
-        return nil, &os.PathError{"stat", filename, err}
-    }
-    return &fs, nil
+	var fs syscall.Stat_t
+	err := syscall.Stat(filename, &fs)
+	if err != nil {
+		return nil, &os.PathError{"stat", filename, err}
+	}
+	return &fs, nil
 }
 
 func fileMode(fs *syscall.Stat_t) os.FileMode {
-    switch fs.Mode & syscall.S_IFMT {
-    case syscall.S_IFBLK:
-        return os.ModeDevice
-    case syscall.S_IFCHR:
-        return os.ModeDevice | os.ModeCharDevice
-    case syscall.S_IFDIR:
-        return os.ModeDir
-    case syscall.S_IFIFO:
-        return os.ModeNamedPipe
-    case syscall.S_IFLNK:
-        return os.ModeSymlink
-    case syscall.S_IFSOCK:
-        return os.ModeSocket
-    }
+	switch fs.Mode & syscall.S_IFMT {
+	case syscall.S_IFBLK:
+		return os.ModeDevice
+	case syscall.S_IFCHR:
+		return os.ModeDevice | os.ModeCharDevice
+	case syscall.S_IFDIR:
+		return os.ModeDir
+	case syscall.S_IFIFO:
+		return os.ModeNamedPipe
+	case syscall.S_IFLNK:
+		return os.ModeSymlink
+	case syscall.S_IFSOCK:
+		return os.ModeSocket
+	}
 
-    /*
-    if fs.Mode&syscall.S_ISGID != 0 {
-        return os.ModeSetgid
-    }
-    if fs.Mode&syscall.S_ISUID != 0 {
-        return os.ModeSetuid
-    }
-    if fs.Mode&syscall.S_ISVTX != 0 {
-        return os.ModeSticky
-    }
-    */
-    return 0
+	/*
+	   if fs.Mode&syscall.S_ISGID != 0 {
+	       return os.ModeSetgid
+	   }
+	   if fs.Mode&syscall.S_ISUID != 0 {
+	       return os.ModeSetuid
+	   }
+	   if fs.Mode&syscall.S_ISVTX != 0 {
+	       return os.ModeSticky
+	   }
+	*/
+	return 0
 }
 
 func WriteFile(filename string, data []byte, append bool) error {
-    flag := os.O_WRONLY | os.O_CREATE
-    if append {
-        flag = flag | os.O_APPEND
-    } else {
-        flag = flag | os.O_TRUNC
-    }
-    f, err := os.OpenFile(filename, flag, FILE_MODE)
-    if err != nil {
-        return err
-    }
-    _, err = f.Write(data)
+	flag := os.O_WRONLY | os.O_CREATE
+	if append {
+		flag = flag | os.O_APPEND
+	} else {
+		flag = flag | os.O_TRUNC
+	}
+	f, err := os.OpenFile(filename, flag, FILE_MODE)
+	if err != nil {
+		return err
+	}
+	_, err = f.Write(data)
 
-    if err1 := f.Close(); err == nil {
-        err = err1
-    }
+	if err1 := f.Close(); err == nil {
+		err = err1
+	}
 
-    return err
+	return err
 }
 
 func CopyRW(reader io.Reader, writer io.Writer, close bool) (uint64, error) {
-
+	return 0, nil
 }
