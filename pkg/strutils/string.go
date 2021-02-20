990f45284b56d29b61b5aa5a7664b5d13f6eed94
zuodengpeng <cqzuodp@163.com>
2021-02-20 22:31:13 +0800

    fix: option function objects.

--- pkg/strutils/string.go
+++ pkg/strutils/string.go
@@ -1,50 +1,58 @@
 package strutils
 
 import (
-    "math/rand"
-    "time"
-    "unsafe"
+	"math/rand"
+	"time"
+	"unsafe"
 )
 
 const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 const alphalen = len(alphanum)
 
 func SubString(str string, start int) string {
-    if start < len(str) {
-        return str[start:]
-    }
-    return ""
+	if start < len(str) {
+		return str[start:]
+	}
+	return ""
 }
 
 func Bytes2String(b []byte) string {
-    return *(*string)(unsafe.Pointer(&b))
+	/*
+	   // 写的第一种方式:
+	       var hdr reflect.StringHeader
+	       hdr.Data = uintptr(unsafe.Pointer(&b))
+	       hdr.Len = len(b)
+	       return **(**string)(unsafe.Pointer(&hdr))
+	*/
+	// 写的第二种方式:
+	return *(*string)(unsafe.Pointer(&b)) // b => slice {addr unsafe.Pointer, len int , cap int }
 }
 
 func String2Bytes(s string) []byte {
-    x := (*[2]uintptr)(unsafe.Pointer(&s)) // stringstruct { str unsafe.Pointer, len int }
-    h := [3]uintptr{x[0], x[1], x[1]}      // slice {addr unsafe.Pointer, len int , cap int }
-    return *(*[]byte)(unsafe.Pointer(&h))
+	x := (*[2]uintptr)(unsafe.Pointer(&s)) // string struct { str unsafe.Pointer, len int }
+	h := [3]uintptr{x[0], x[1], x[1]}      // slice {addr unsafe.Pointer, len int , cap int }
+	return *(*[]byte)(unsafe.Pointer(&h))
 }
 
 func RandomString(len int) string {
-    r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-    var bytes = make([]byte, len)
-    n, e := r.Read(bytes)
+	var bytes = make([]byte, len)
+	n, e := r.Read(bytes)
 
-    dir := false
-    if n == len && e == nil {
-        dir = true
-    }
-    if dir {
-        for i, b := range bytes {
-            bytes[i] = alphanum[b%byte(alphalen)]
-        }
-    } else {
-        for i := 0; i < len; i++ {
-            bytes[i] = alphanum[r.Int()%alphalen]
-        }
-    }
+	dir := false
+	if n == len && e == nil {
+		dir = true
+	}
+	if dir {
+		for i, b := range bytes {
+			bytes[i] = alphanum[b%byte(alphalen)]
+		}
+	} else {
+		for i := 0; i < len; i++ {
+			bytes[i] = alphanum[r.Int()%alphalen]
+		}
+	}
 
-    return string(bytes)
+	return string(bytes)
 }
--- pkg/timeutils/timefmt_test.go
+++ pkg/timeutils/timefmt_test.go
@@ -1,11 +1,16 @@
 package timeutils
 
 import (
+	"github.com/ezzuodp/go-lib/pkg/strutils"
 	"testing"
 	"time"
 )
 
 func TestFmtDate(t *testing.T) {
+	s := "aaaaaaa"
+	bytes := []byte(s)
+	v := strutils.Bytes2String(bytes[:])
+	t.Logf("ssss=>[%s]", v)
 	now := time.Now()
 	t.Log(NowUnix())
 	t.Log(NowMillis())
